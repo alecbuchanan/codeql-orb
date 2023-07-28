0b9e079a42814455b904c5921639a42e5ed48f1f
alecbuchanan <alecbuchanan@github.com>
2023-07-28 12:30:45 -0700

    feat: adding init command for linux runners

---
+++ src/scripts/download_cli.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Download and extract the CodeQL CLI
+curl -L https://github.com/github/codeql-cli-binaries/releases/latest/download/codeql-linux64.zip -o codeql.zip
+unzip codeql.zip -d /usr/local/bin/
+rm codeql.zip
+
+# Add the CodeQL CLI to the PATH
+echo 'export PATH=$PATH:/usr/local/bin/codeql-linux64' >> $BASH_ENV
+source $BASH_ENV
