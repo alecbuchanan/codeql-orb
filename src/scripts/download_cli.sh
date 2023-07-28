9f2aa1acbaaa7a2f3bd6c3bf37d2b720424da5bf
alecbuchanan <alecbuchanan@github.com>
2023-07-28 12:38:29 -0700

    Adding double quotes to BASH_ENV

--- src/scripts/download_cli.sh
+++ src/scripts/download_cli.sh
@@ -6,5 +6,5 @@ unzip codeql.zip -d /usr/local/bin/
 rm codeql.zip
 
 # Add the CodeQL CLI to the PATH
-echo 'export PATH=$PATH:/usr/local/bin/codeql-linux64' >> $BASH_ENV
+echo 'export PATH=$PATH:/usr/local/bin/codeql-linux64' >> "$BASH_ENV"
 source "$BASH_ENV"
