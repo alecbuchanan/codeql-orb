68bfa582562271e3e313e036ed0c225078d3e1df
alecbuchanan <alecbuchanan@github.com>
2023-09-05 21:09:06 -0400

    Adding CodeQL bundle download

--- src/scripts/download_cli.sh
+++ src/scripts/download_cli.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# Download and extract the CodeQL CLI
-sudo curl -L https://github.com/github/codeql-cli-binaries/releases/latest/download/codeql-linux64.zip -o codeql.zip
-sudo unzip codeql.zip -d /usr/local/bin/
-sudo rm codeql.zip
+# Download and extract the CodeQL bundle
+sudo curl -L https://github.com/github/codeql/releases/latest/download/codeql-bundle.tar.gz -o codeql-bundle.tar.gz
+sudo tar -xzf codeql-bundle.tar.gz -C /usr/local/
+sudo rm codeql-bundle.tar.gz
 
 # Add the CodeQL CLI to the PATH
-echo "export PATH=\$PATH:/usr/local/bin/codeql-linux64" | sudo tee -a "$BASH_ENV" > /dev/null
+echo "export PATH=\$PATH:/usr/local/codeql-runner-linux64" | sudo tee -a "$BASH_ENV" > /dev/null
 # shellcheck source=/dev/null
 source "$BASH_ENV"
