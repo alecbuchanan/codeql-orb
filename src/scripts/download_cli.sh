5bf71dfb819263acc6c687592578f854f141446a
alecbuchanan <alecbuchanan@github.com>
2023-09-05 20:58:45 -0400

    Adding sudo

--- src/scripts/download_cli.sh
+++ src/scripts/download_cli.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Download and extract the CodeQL CLI
-curl -L https://github.com/github/codeql-cli-binaries/releases/latest/download/codeql-linux64.zip -o codeql.zip
-unzip codeql.zip -d /usr/local/bin/
-rm codeql.zip
+sudo curl -L https://github.com/github/codeql-cli-binaries/releases/latest/download/codeql-linux64.zip -o codeql.zip
+sudo unzip codeql.zip -d /usr/local/bin/
+sudo rm codeql.zip
 
 # Add the CodeQL CLI to the PATH
-echo "export PATH=\$PATH:/usr/local/bin/codeql-linux64" >> "$BASH_ENV"
+echo "export PATH=\$PATH:/usr/local/bin/codeql-linux64" | sudo tee -a "$BASH_ENV" > /dev/null
 # shellcheck source=/dev/null
 source "$BASH_ENV"
