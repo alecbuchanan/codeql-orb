98900d365b3901ff5f95ca81e0fdf9854974be71
alecbuchanan <alecbuchanan@github.com>
2023-09-06 20:17:10 -0400

    suuudowoodoo

--- src/scripts/download_bundle.sh
+++ src/scripts/download_bundle.sh
@@ -2,7 +2,7 @@
 
 # Download and extract the CodeQL bundle
 wget https://github.com/github/codeql-action/releases/latest/download/codeql-bundle-linux64.tar.gz -O codeql-bundle-linux64.tar.gz
-tar -xzvf codeql-bundle-linux64.tar.gz -C /usr/local/
+sudo tar -xzvf codeql-bundle-linux64.tar.gz -C /usr/local/
 rm codeql-bundle-linux64.tar.gz
 
 # Add the CodeQL CLI to the PATH
