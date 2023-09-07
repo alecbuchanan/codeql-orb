f2dd35593762aa35d96645b02fbf0619129ffa75
alecbuchanan <alecbuchanan@github.com>
2023-09-06 20:27:08 -0400

    Updating path ref

--- src/scripts/create_database.sh
+++ src/scripts/create_database.sh
@@ -2,4 +2,4 @@
 
 ##todo, make this a parameter
 mkdir codeql-dbs
-./codeql/codeql database create ./codeql-dbs/repo-db --language=javascript --source-root=./my-app
+/usr/local/codeql-runner-linux64/codeql database create ./codeql-dbs/repo-db --language=javascript --source-root=./my-app
