7dcfd1319f490b02f233560f60d1ab88fe94a7f3
alecbuchanan <alecbuchanan@github.com>
2023-09-06 20:09:26 -0400

    Fixing filename

--- src/scripts/download_bundle.sh
+++ src/scripts/download_bundle.sh
@@ -2,8 +2,8 @@
 
 # Download and extract the CodeQL bundle
 wget https://github.com/github/codeql-action/releases/latest/download/codeql-bundle-linux64.tar.gz -o codeql-bundle-linux64.tar.gz
-tar -xzvf codeql-bundle.tar.gz -C /usr/local/
-rm codeql-bundle.tar.gz
+tar -xzvf codeql-bundle-linux64.tar.gz -C /usr/local/
+rm codeql-bundle-linux64.tar.gz
 
 # Add the CodeQL CLI to the PATH
 echo "export PATH=\$PATH:/usr/local/codeql-runner-linux64" | sudo tee -a "$BASH_ENV" > /dev/null
