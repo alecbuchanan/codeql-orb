371797de9feb29c87525e213c351a070dabcc5da
alecbuchanan <alecbuchanan@github.com>
2023-09-20 11:41:04 -0400

    Fixing org/repo reference

--- src/scripts/upload_pr_results.sh
+++ src/scripts/upload_pr_results.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "$GITHUB_TOKEN" | ./codeql/codeql github upload-results --repository=org/example-app --ref="refs/pull/${CIRCLE_PULL_REQUEST##*/}/head" --commit="$CIRCLE_SHA1" --sarif="./codeql/temp/results-js.sarif" --github-url="https://github.com/" --github-auth-stdin
+echo "$GITHUB_TOKEN" | ./codeql/codeql github upload-results --repository=alecbuchanan/codeql-orb --ref="refs/pull/${CIRCLE_PULL_REQUEST##*/}/head" --commit="$CIRCLE_SHA1" --sarif="./codeql/temp/results-js.sarif" --github-url="https://github.com/" --github-auth-stdin
