64cad66c24c6c1f9af7df0fedf1b9503cbc5941b
alecbuchanan <alecbuchanan@github.com>
2024-05-19 14:20:48 -0400

    chore: renaming LANGUAGE to LANGUAGE_IDENTIFIER

--- src/scripts/create_database.sh
+++ src/scripts/create_database.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Ensure the LANGUAGE environment variable is set
-if [ -z "$LANGUAGE" ]; then
+# Ensure the LANGUAGE_IDENTIFIER environment variable is set
+if [ -z "$LANGUAGE_IDENTIFIER" ]; then
     echo "Error: LANGUAGE environment variable is not set."
     exit 1
 fi
 
-# Use the LANGUAGE environment variable directly without assigning it to another variable
+# Use the LANGUAGE_IDENTIFIER environment variable directly without assigning it to another variable
 
 # Create the directory for CodeQL databases if it doesn't already exist
 mkdir -p codeql-dbs
 
 # Use the language from the environment variable to create a CodeQL database
-./codeql/codeql database create ./codeql-dbs/repo-db --language="$LANGUAGE" --source-root=./my-app
+./codeql/codeql database create ./codeql-dbs/repo-db --language="$LANGUAGE_IDENTIFIER" --source-root=./my-app
