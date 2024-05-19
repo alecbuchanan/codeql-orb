585d7b40a74569044c44b214cdd32c7995d1713f
alecbuchanan <alecbuchanan@github.com>
2024-05-19 15:59:52 -0400

    chore: extracting envar to bash_env

--- src/scripts/parse_repository.sh
+++ src/scripts/parse_repository.sh
@@ -3,5 +3,8 @@
 # Extract the "organization/repo" format from the CIRCLE_REPOSITORY_URL environment variable
 repo=$(echo "$CIRCLE_REPOSITORY_URL" | awk -F'[:/]' '{print $2"/"$3}' | sed 's/\.git$//')
 
+# Export the repo variable for all subsequent steps
+echo "export repo='$repo'" >> $BASH_ENV
+
 # Print the extracted repository information
 echo "Extracted Repository: $repo"
