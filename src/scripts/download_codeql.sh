d30ca7e594b721f178ede86aa3f6f5aec648e747
alecbuchanan <alecbuchanan@github.com>
2024-05-19 18:18:13 -0400

    chore: saving codeql cli outside of project root

--- src/scripts/download_codeql.sh
+++ src/scripts/download_codeql.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+# Define the directory for CodeQL
+CODEQL_DIR="/home/circleci/codeql"
+
+# Create the CodeQL directory if it doesn't exist
+mkdir -p "$CODEQL_DIR"
+
 # Download the latest CodeQL bundle
 wget https://github.com/github/codeql-action/releases/latest/download/codeql-bundle-linux64.tar.gz -O codeql-bundle-linux64.tar.gz
 
-# Extract the downloaded tar.gz file
-tar xzvf codeql-bundle-linux64.tar.gz
+# Extract the downloaded tar.gz file into the specified directory
+tar xzvf codeql-bundle-linux64.tar.gz -C "$CODEQL_DIR" --strip-components=1
 
 # Remove the tar.gz file after extraction
 rm codeql-bundle-linux64.tar.gz
 
-# Declare the CODEQL_DIR variable and assign it separately to avoid masking values
-CODEQL_DIR=$(pwd)/codeql
-export PATH=$CODEQL_DIR:$PATH
+# Add the CodeQL directory to your PATH for the current session
+export PATH="$CODEQL_DIR:$PATH"
+
+# Make the PATH change persistent across sessions by adding it to ~/.bashrc
+echo 'export PATH="/home/circleci/codeql:$PATH"' >> ~/.bashrc
+
+# Now you can run the CodeQL executable directly with 'codeql'
