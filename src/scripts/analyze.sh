ffe722fdb6d011d0a284ebce977b0318fb8193b2
alecbuchanan <alecbuchanan@github.com>
2024-05-19 15:19:08 -0400

    chore: print sarif file path in analyze.sh

--- src/scripts/analyze.sh
+++ src/scripts/analyze.sh
@@ -6,3 +6,6 @@ mkdir -p temp
 
 # Run the CodeQL database analyze command
 ./codeql database analyze ../codeql-dbs/repo-db javascript-code-scanning.qls --format=sarif-latest --output=./temp/results-js.sarif
+
+# Print the full path of the SARIF file for debugging
+echo "SARIF file location: $(pwd)/temp/results-js.sarif"
