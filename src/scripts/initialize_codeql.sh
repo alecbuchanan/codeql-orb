6d6cd565f2f8a4b76172b7e9bce3ebc96d474753
alecbuchanan <alecbuchanan@github.com>
2024-06-01 16:06:21 -0400

    chore: fixing bug

--- src/scripts/initialize_codeql.sh
+++ src/scripts/initialize_codeql.sh
@@ -28,13 +28,13 @@ cd "$CODEQL_DIR" || exit 1
 mkdir -p codeql-dbs
 
 # Use the language from the environment variable to create a CodeQL database
-if [ -z "$BUILD_MODE" ]; then
+if [ -n "$BUILD_MODE" ]; then
   CMD="./codeql database create ./codeql-dbs/repo-db --language=\"$LANGUAGE_IDENTIFIER\" --source-root /home/circleci/project"
 else
   CMD="./codeql database create ./codeql-dbs/repo-db --language=\"$LANGUAGE_IDENTIFIER\" --source-root /home/circleci/project --build-mode=\"$BUILD_MODE\""
 fi
 
-if [ -z "$COMMAND" ]; then
+if [ -n "$COMMAND" ]; then
   CMD="$CMD --command=\"$COMMAND\""
 fi
 
