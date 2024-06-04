a9ce49b7f86a794005e12ad5846e2981835c0b0b
alecbuchanan <alecbuchanan@github.com>
2024-06-04 10:53:24 -0400

    trying copilot's recommendations

--- src/scripts/initialize_codeql.sh
+++ src/scripts/initialize_codeql.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 # Ensure the LANGUAGE_IDENTIFIER and CODEQL_DIR environment variable is set
 if [ -z "$LANGUAGE_IDENTIFIER" ] || [ -z "$CODEQL_DIR" ]; then
@@ -6,16 +7,19 @@ if [ -z "$LANGUAGE_IDENTIFIER" ] || [ -z "$CODEQL_DIR" ]; then
   exit 1
 fi
 
-# Check if the user is root https://circleci.com/docs/orbs-best-practices/#check-for-root
-if [[ $EUID == 0 ]]; then export SUDO=""; else # Check if we are root
-  export SUDO="sudo";
+# Check if the user is root
+if [[ $EUID == 0 ]]; then export SUDO=""; else
+  # Check if we have write access to the necessary directories
+  if [ ! -w "$CODEQL_DIR" ] || [ ! -w codeql-dbs ]; then
+    export SUDO="sudo";
+  fi
 fi
 
 # Create the CodeQL directory if it doesn't exist
 $SUDO mkdir -p "$CODEQL_DIR"
 
 # Download the latest CodeQL bundle
-$SUDO wget https://github.com/github/codeql-action/releases/latest/download/codeql-bundle-linux64.tar.gz -O codeql-bundle-linux64.tar.gz
+$SUDO curl -LO https://github.com/github/codeql-action/releases/latest/download/codeql-bundle-linux64.tar.gz
 
 # Extract the downloaded tar.gz file into the specified directory
 $SUDO tar xzvf codeql-bundle-linux64.tar.gz -C "$CODEQL_DIR" --strip-components=1
@@ -37,13 +41,10 @@ $SUDO chown -R "$(whoami)" codeql-dbs
 
 # Use the language from the environment variable to create a CodeQL database
 CMD="./codeql database create ./codeql-dbs/repo-db --language=\"$LANGUAGE_IDENTIFIER\" --source-root /home/circleci/project"
-
 if [ -n "$BUILD_MODE" ]; then
   CMD="$CMD --build-mode=\"$BUILD_MODE\""
 fi
-
 if [ -n "$COMMAND" ]; then
   CMD="$CMD --command=\"$COMMAND\""
 fi
-
 sh -c "$CMD"
